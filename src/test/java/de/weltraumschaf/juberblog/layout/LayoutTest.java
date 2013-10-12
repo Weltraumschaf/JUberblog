c02f629a24d57eb4b97fc61001b0bd06d0252f33
Weltraumschaf <ich@weltraumschaf.de>
2013-10-13 01:38:19 +0200

    Implement layout with template.

--- src/test/java/de/weltraumschaf/juberblog/layout/LayoutTest.java
+++ src/test/java/de/weltraumschaf/juberblog/layout/LayoutTest.java
@@ -9,9 +9,24 @@
  *
  * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
  */
-
 package de.weltraumschaf.juberblog.layout;
 
+import com.google.common.collect.Maps;
+import de.weltraumschaf.juberblog.Constants;
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.Version;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.URISyntaxException;
+import java.util.Map;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.Validate;
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -22,22 +37,86 @@ import static org.hamcrest.Matchers.*;
  */
 public class LayoutTest {
 
-    @Test public void renderLAyout() {
-        final Layout layout = new Layout("layout.fmt");
-        assertThat(layout.render("content"), is(equalTo("")));
+    /**
+     * Freemarker templates resource directory.
+     */
+    private static final String TEMPLATE_DIRECTORRY = "/de/weltraumschaf/juberblog/layout";
+    /**
+     * Major version of Freemarker template engine.
+     */
+    private static final int FREEMARKER_MAJOR_VERSION = 2;
+    /**
+     * Minor version of Freemarker template engine.
+     */
+    private static final int FREEMARKER_MINOR_VERSION = 3;
+    /**
+     * Mini version of Freemarker template engine.
+     */
+    private static final int FREEMARKER_MINI_VERSION = 20;
+    /**
+     * Used Freemarker version.
+     */
+    private static final Version FREEMARKER_VERSION = new Version(
+            FREEMARKER_MAJOR_VERSION,
+            FREEMARKER_MINOR_VERSION,
+            FREEMARKER_MINI_VERSION);
+    private static final String ENCODING = "UTF-8";
+
+    @Test
+    public void renderLAyout() throws IOException, URISyntaxException, TemplateException {
+        final Layout layout = new Layout(configureTemplates(), "layout.ftl");
+        layout.setTitle("foo");
+        layout.setDescription("bar");
+        final InputStream htmlFile = getClass().getResourceAsStream(TEMPLATE_DIRECTORRY + "/layout.html");
+        assertThat(layout.render("baz"), is(equalTo(IOUtils.toString(htmlFile))));
+        IOUtils.closeQuietly(htmlFile);
+    }
+
+    private Configuration configureTemplates() throws IOException, URISyntaxException {
+        final Configuration cfg = new Configuration();
+        final File templateDir = new File(getClass().getResource(TEMPLATE_DIRECTORRY).toURI());
+        cfg.setDirectoryForTemplateLoading(templateDir);
+        cfg.setObjectWrapper(new DefaultObjectWrapper());
+        cfg.setDefaultEncoding(ENCODING);
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+//        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+        cfg.setIncompatibleImprovements(FREEMARKER_VERSION);
+        return cfg;
     }
 
     public class Layout {
-        private final String templateFilename;
 
-        public Layout(final String templateFilename) {
+        private final String templateFile;
+        private final Configuration configuration;
+        private String title = "";
+        private String description = "";
+
+        public Layout(final Configuration configuration, final String templateFile) {
             super();
-            this.templateFilename = templateFilename;
+            this.configuration = configuration;
+            this.templateFile = templateFile;
         }
 
-        public String render(final String content) {
-            return "";
+        public void setTitle(String title) {
+            Validate.notNull(title);
+            this.title = title;
         }
 
+        public void setDescription(final String description) {
+            Validate.notNull(description);
+            this.description = description;
+        }
+
+        public String render(final String content) throws IOException, TemplateException {
+            final freemarker.template.Template tpl = configuration.getTemplate(templateFile);
+            final ByteArrayOutputStream out = new ByteArrayOutputStream();
+            final Map<String, Object> templateVariables = Maps.newHashMap();
+            templateVariables.put("content", content);
+            templateVariables.put("title", title);
+            templateVariables.put("description", description);
+            templateVariables.put("encoding", configuration.getDefaultEncoding());
+            tpl.process(templateVariables, new OutputStreamWriter(out, configuration.getDefaultEncoding()));
+            return out.toString(configuration.getDefaultEncoding());
+        }
     }
 }
---
+++ src/test/resources/de/weltraumschaf/juberblog/layout/layout.ftl
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>${title}</title>
+        <meta name="description" content="${description}"/>
+        <meta charset="${encoding}"/>
+        <style>
+
+        </style>
+    </head>
+    <body>
+        ${content}
+    </body>
+</html>
