02aaa797d076483398af534cb25511474fef419e
Weltraumschaf <ich@weltraumschaf.de>
2013-10-15 22:32:59 +0200

    Add test stubs.

---
+++ src/test/java/de/weltraumschaf/juberblog/AppTest.java
@@ -0,0 +1,30 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+package de.weltraumschaf.juberblog;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Tests for {@link App}.
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class AppTest {
+
+    @Test @Ignore("TODO Implement tests.")
+    public void testSomeMethod() {
+    }
+
+}
