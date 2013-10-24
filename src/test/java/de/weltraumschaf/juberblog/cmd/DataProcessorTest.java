259bb136448d2d7762889d32e330cc0f3c8bae48
Sven Strittmatter <Sven.Strittmatter@icongmbh.de>
2013-10-24 18:02:04 +0200

    Add test class.

---
+++ src/test/java/de/weltraumschaf/juberblog/cmd/DataProcessorTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.weltraumschaf.juberblog.cmd;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests for {@link DataProcessor}.
+ *
+ * @author Sven.Strittmatter
+ */
+public class DataProcessorTest {
+
+    @Test
+    public void testSomeMethod() {
+    }
+}
