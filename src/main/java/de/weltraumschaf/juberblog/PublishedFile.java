d029d5e0653614977db162ff70a23c9a58be1f26
Weltraumschaf <ich@weltraumschaf.de>
2013-10-27 10:56:38 +0100

    Abtraction for published file.

---
+++ src/main/java/de/weltraumschaf/juberblog/PublishedFile.java
@@ -0,0 +1,66 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+package de.weltraumschaf.juberblog;
+
+import java.io.File;
+import org.apache.commons.lang3.Validate;
+
+/**
+ * Abstracts a published file.
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class PublishedFile {
+
+    /**
+     * Base URI to decorate the file base name.
+     */
+    private final String baseUri;
+    /**
+     * Originating file.
+     */
+    private final File file;
+
+    /**
+     * Dedicated constructor.
+     *
+     * @param baseUri must not be {@code null}
+     * @param file must not be {@code null}
+     */
+    public PublishedFile(final String baseUri, final File file) {
+        super();
+        Validate.notNull(baseUri, "BaseUri must not be null!");
+        Validate.notNull(file, "File must not be null!");
+        this.baseUri = baseUri;
+        this.file = file;
+    }
+
+    /**
+     * Get combination of {@link #baseUri} and file name.
+     *
+     * @return never {@code null}
+     */
+    public String getUri() {
+        return baseUri + file.getName();
+    }
+
+    /**
+     * @see java.io.File.lastModified():long
+     * @return A <code>long</code> value representing the time the file was
+     *          last modified, measured in milliseconds since the epoch
+     *          (00:00:00 GMT, January 1, 1970), or <code>0L</code> if the
+     *          file does not exist or if an I/O error occurs
+     */
+    public long getModificationTime() {
+        return file.lastModified();
+    }
+}
