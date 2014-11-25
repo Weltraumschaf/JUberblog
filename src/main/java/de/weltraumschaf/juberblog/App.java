af545dba95252efb7301f8cabb60d25e45dde445
Sven Strittmatter <ich@weltraumschaf.de>
2014-11-25 23:18:37 +0100

    Add main class.

---
+++ src/main/java/de/weltraumschaf/juberblog/App.java
@@ -0,0 +1,31 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+package de.weltraumschaf.juberblog;
+
+/**
+ * Main class invoked by JVM.
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public final class App {
+
+    /**
+     * Main entry point of VM.
+     *
+     * @param args CLI arguments from VM
+     */
+    public static void main(final String[] args) {
+
+    }
+
+}
