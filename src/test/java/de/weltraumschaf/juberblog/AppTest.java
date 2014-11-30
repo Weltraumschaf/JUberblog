426722b2d6c2c102eeb17cdf788ef37756d660f2
Sven Strittmatter <ich@weltraumschaf.de>
2014-11-30 02:48:13 +0100

    Render a post.

--- src/test/java/de/weltraumschaf/juberblog/AppTest.java
+++ src/test/java/de/weltraumschaf/juberblog/AppTest.java
@@ -14,13 +14,14 @@ package de.weltraumschaf.juberblog;
 import de.weltraumschaf.commons.guava.Maps;
 import de.weltraumschaf.freemarkerdown.Fragment;
 import de.weltraumschaf.freemarkerdown.FreeMarkerDown;
+import de.weltraumschaf.freemarkerdown.Layout;
+import de.weltraumschaf.freemarkerdown.Options;
 import de.weltraumschaf.freemarkerdown.PreProcessor;
 import de.weltraumschaf.freemarkerdown.PreProcessors;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 import static org.hamcrest.Matchers.is;
@@ -34,26 +35,47 @@ import org.junit.Test;
  */
 public class AppTest {
 
+    private static final String BASE = "/de/weltraumschaf/juberblog/";
+    private static final String ENCODING = "utf-8";
+
+    private Path createPath(final String name) throws URISyntaxException {
+        return Paths.get(getClass().getResource(BASE + name).toURI());
+    }
+
     @Test
     public void testSomeMethod() throws URISyntaxException, UnsupportedEncodingException, IOException {
         final FreeMarkerDown fmd = FreeMarkerDown.create();
-        final URI input = getClass().getResource("/de/weltraumschaf/juberblog/2014-05-30T21.29.20_This-is-the-First-Post.md").toURI();
-        final Fragment content = fmd.createFragemnt(new String(Files.readAllBytes(Paths.get(input)), "utf-8"));
+
+        final Fragment content = fmd.createFragemnt(createPath("2014-05-30T21.29.20_This-is-the-First-Post.md"), ENCODING);
+
+        final Layout post = fmd.createLayout(createPath("post.ftl"), ENCODING, Options.WITHOUT_MARKDOWN);
+        post.assignTemplateModel("content", content);
+
+        final Layout layout = fmd.createLayout(createPath("layout.ftl"), ENCODING, Options.WITHOUT_MARKDOWN);
+        layout.assignVariable("name", "NAME");
+        layout.assignVariable("description", "DESCRIPTION");
+        layout.assignTemplateModel("content", post);
 
         final Map<String, String> keyValues = Maps.newHashMap();
         final PreProcessor processor = PreProcessors.createKeyValueProcessor(keyValues);
         fmd.register(processor);
 
-        assertThat(fmd.render(content), is(
-                "<h2>This is the First Post</h2>"
-                + "<p>Lorem ipsum dolor sit amet consetetur sadipscing elitr sed diam nonumy eirmod "
-                + "tempor invidunt ut labore et dolore magna aliquyam erat sed diam voluptua at vero "
-                + "eos et accusam et justo duo dolores et ea rebum stet clita kasd gubergren no sea "
-                + "takimata sanctus est lorem ipsum dolor sit amet.</p>"
-                + "<p>Lorem ipsum dolor sit amet consetetur sadipscing elitr sed diam nonumy eirmod "
-                + "tempor invidunt ut labore et dolore magna aliquyam erat sed diam voluptua at vero "
-                + "eos et accusam et justo duo dolores et ea rebum stet clita kasd gubergren no sea "
-                + "takimata sanctus est lorem ipsum dolor sit amet.</p>"));
+        assertThat(fmd.render(layout), is(
+                "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "    <body>\n"
+                + "        <h1>NAME</h1>\n"
+                + "        <h2>DESCRIPTION</h2>\n"
+                + "\n"
+                + "        <article>\n"
+                + "    <h3>This is the First Post</h3>"
+                + "<p>Lorem ipsum dolor sit amet consetetur sadipscing elitr sed diam nonumy eirmod tempor "
+                + "invidunt ut labore et dolore magna aliquyam.</p>"
+                + "<p>Lorem ipsum dolor sit amet consetetur sadipscing elitr sed diam nonumy eirmod tempor "
+                + "invidunt ut labore et dolore magna aliquyam.</p>\n"
+                + "</article>\n"
+                + "    </body>\n"
+                + "</html>"));
     }
 
 }
