a97d7146038d17c42edea7cfd046ebc5516242f0
Sven Strittmatter <ich@weltraumschaf.de>
2014-11-30 15:07:25 +0100

    Extract mardown file finder in own class.

--- src/test/java/de/weltraumschaf/juberblog/AppTest.java
+++ src/test/java/de/weltraumschaf/juberblog/AppTest.java
@@ -12,6 +12,7 @@
 package de.weltraumschaf.juberblog;
 
 import de.weltraumschaf.commons.guava.Maps;
+import de.weltraumschaf.commons.validate.Validate;
 import de.weltraumschaf.freemarkerdown.FreeMarkerDown;
 import de.weltraumschaf.freemarkerdown.Layout;
 import de.weltraumschaf.freemarkerdown.Options;
@@ -25,8 +26,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
@@ -69,27 +74,18 @@ public class AppTest {
     }
 
     @Test
-    public void findFiles() throws URISyntaxException {
-        final DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
-            @Override
-            public boolean accept(final Path file) throws IOException {
-                return file.toString().endsWith(".md");
-            }
-        };
+    public void findFiles() throws URISyntaxException, IOException {
+        final Collection<DataFile> foundFiles = new MarkdownFilesFinder().find(createPath("posts"));
 
-        final List<String> fileNames = new ArrayList<>();
-
-        try (final DirectoryStream<Path> directoryStream = Files.newDirectoryStream(createPath("posts"), filter)) {
-            for (final Path path : directoryStream) {
-                fileNames.add(path.toString());
-            }
-        } catch (IOException ex) {
-        }
-
-        assertThat(fileNames.size(), is(3));
+        assertThat(foundFiles.size(), is(3));
+        assertThat(foundFiles, containsInAnyOrder(
+                new DataFile(createPath("posts/2014-05-30T21.29.20_This-is-the-First-Post.md").toString()),
+                new DataFile(createPath("posts/2014-06-30T23.25.44_This-is-the-Second-Post.md").toString()),
+                new DataFile(createPath("posts/2014-07-28T17.44.13_This-is-the-Third-Post.md").toString())
+        ));
     }
 
-    final class Renderer {
+    final static class Renderer {
 
         private static final String ENCODING = "utf-8";
 
@@ -117,4 +113,60 @@ public class AppTest {
         }
     }
 
+    final static class DataFile {
+
+        private final String absoluteFileName;
+
+        DataFile(final String absoluteFileName) {
+            super();
+            this.absoluteFileName = Validate.notEmpty(absoluteFileName, "absoluteFileName");
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(this.absoluteFileName);
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (!(obj instanceof DataFile)) {
+                return false;
+            }
+
+            final DataFile other = (DataFile) obj;
+
+            return Objects.equals(this.absoluteFileName, other.absoluteFileName);
+        }
+
+        @Override
+        public String toString() {
+            return "DataFile{" + "absoluteFileName=" + absoluteFileName + '}';
+        }
+
+    }
+
+    final static class MarkdownFilesFinder {
+
+        private static final MarkdownFileFinder FILTER = new MarkdownFileFinder();
+
+        Collection<DataFile> find(final Path directory) throws IOException {
+            final List<DataFile> foundFiles = new ArrayList<>();
+
+            try (final DirectoryStream<Path> directoryStream = Files.newDirectoryStream(directory, FILTER)) {
+                for (final Path path : directoryStream) {
+                    foundFiles.add(new DataFile(path.toString()));
+                }
+            }
+
+            return Collections.unmodifiableList(foundFiles);
+        }
+
+        private static final class MarkdownFileFinder implements DirectoryStream.Filter<Path> {
+
+            @Override
+            public boolean accept(final Path file) throws IOException {
+                return file.toString().endsWith(".md");
+            }
+        }
+    }
 }
