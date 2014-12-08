370feb9ae0d580f386ef1014fa0098ca9fb4718a
Sven Strittmatter <ich@weltraumschaf.de>
2014-12-08 22:52:52 +0100

    Extract index generation task.

--- src/test/java/de/weltraumschaf/juberblog/AppTest.java
+++ src/test/java/de/weltraumschaf/juberblog/AppTest.java
@@ -11,27 +11,15 @@
  */
 package de.weltraumschaf.juberblog;
 
-import com.beust.jcommander.internal.Maps;
-import de.weltraumschaf.commons.guava.Lists;
-import de.weltraumschaf.commons.validate.Validate;
-import de.weltraumschaf.freemarkerdown.Fragment;
-import de.weltraumschaf.freemarkerdown.FreeMarkerDown;
-import de.weltraumschaf.freemarkerdown.Layout;
-import de.weltraumschaf.freemarkerdown.RenderOptions;
-import de.weltraumschaf.juberblog.Page.Pages;
 import de.weltraumschaf.juberblog.file.DataFile;
 import de.weltraumschaf.juberblog.file.FilesFinderByExtension;
 import de.weltraumschaf.juberblog.file.FileNameExtension;
 import de.weltraumschaf.juberblog.tasks.GenerateFeedTask;
+import de.weltraumschaf.juberblog.tasks.GenerateIndexTask;
 import de.weltraumschaf.juberblog.tasks.PublishTask;
 import de.weltraumschaf.juberblog.tasks.Task;
 import de.weltraumschaf.juberblog.tasks.TaskExecutor;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import org.joda.time.DateTime;
@@ -157,88 +145,6 @@ public class AppTest extends JUberblogTestCase {
                 + "\n"
                 + "    </body>\n"
                 + "</html>"));
-    }
-
-    static class GenerateIndexTask implements Task<Void, Pages> {
-
-        private final Config config;
-
-        public GenerateIndexTask(final Config config) {
-            super();
-            this.config = Validate.notNull(config, "config");
-        }
-
-        @Override
-        public Class<Pages> getDesiredTypeForPreviusResult() {
-            return Page.Pages.class;
-        }
-
-        @Override
-        public Void execute() throws Exception {
-            return execute(new Pages());
-        }
-
-        @Override
-        public Void execute(Pages previusResult) throws Exception {
-            final FreeMarkerDown fmd = FreeMarkerDown.create(config.encoding);
-            final Fragment template = fmd.createFragemnt(
-                    config.indexTemplate,
-                    config.encoding,
-                    config.indexTemplate.toString(),
-                    RenderOptions.WITHOUT_MARKDOWN);
-            final Layout layout = fmd.createLayout(
-                    config.layoutTemplate,
-                    config.encoding,
-                    config.layoutTemplate.toString(),
-                    RenderOptions.WITHOUT_MARKDOWN);
-            layout.assignTemplateModel("content", template);
-            layout.assignVariable("name", "TODO");
-            layout.assignVariable("description", "TODO");
-            template.assignVariable("posts", convert(previusResult));
-            Files.write(
-                    config.outputDir.resolve("index" + FileNameExtension.HTML.getExtension()),
-                    fmd.render(layout).getBytes(config.encoding)
-            );
-
-            return null;
-        }
-
-        private Collection<Map<String, String>> convert(final List<Page> pages) {
-            final Collection<Map<String, String>> items = Lists.newArrayList();
-
-            for (final Page page : pages) {
-                items.add(convert(page));
-            }
-
-            return Collections.unmodifiableCollection(items);
-        }
-
-        private Map<String, String> convert(final Page page) {
-            final Map<String, String> item = Maps.newHashMap();
-            item.put("title", page.getTitle());
-            item.put("link", page.getLink());
-            item.put("description", page.getDescription());
-            item.put("pubDate", DateFormatter.format(page.getPublishingDate(), DateFormatter.Format.RSS_PUBLISH_DATE_FORMAT));
-            item.put("dcDate", DateFormatter.format(page.getPublishingDate(), DateFormatter.Format.RSS_DC_DATE_FORMAT));
-            return Collections.unmodifiableMap(item);
-        }
-
-        public static final class Config {
-
-            private final String encoding;
-            private final Path outputDir;
-            private final Path layoutTemplate;
-            private final Path indexTemplate;
-
-            public Config(final String encoding, final Path outputDir, final Path layoutTemplate, final Path indexTemplate) {
-                super();
-                this.encoding = Validate.notEmpty(encoding, "encoding");
-                this.outputDir = Validate.notNull(outputDir, "outputDir");
-                this.layoutTemplate = Validate.notNull(layoutTemplate, "layoutTemplate");
-                this.indexTemplate = Validate.notNull(indexTemplate, "indexTemplate");
-            }
-
-        }
     }
 
     class GenerateSitemapTask implements Task<Void, Void> {
