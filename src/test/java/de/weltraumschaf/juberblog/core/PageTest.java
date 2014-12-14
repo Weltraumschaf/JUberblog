d92f38ac71e15b290bebe99ff12309f94f97c5f6
Sven Strittmatter <ich@weltraumschaf.de>
2014-12-14 05:08:06 +0100

    Test to string method.

--- src/test/java/de/weltraumschaf/juberblog/core/PageTest.java
+++ src/test/java/de/weltraumschaf/juberblog/core/PageTest.java
@@ -12,6 +12,9 @@
 package de.weltraumschaf.juberblog.core;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
+import static org.hamcrest.Matchers.is;
+import org.joda.time.DateTime;
+import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 /**
@@ -24,6 +27,13 @@ public class PageTest {
     @Test
     public void equalsContract() {
         EqualsVerifier.forClass(Page.class).verify();
+    }
+
+    @Test
+    public void toStringContainsAllProperties() {
+        final Page sut = new Page("title", "link", "desc", new DateTime(0L), Page.Type.POST);
+
+        assertThat(sut.toString(), is("Page{title=title, link=link, description=desc, publishingDate=1970-01-01T01:00:00.000+01:00, type=POST}"));
     }
 
 }
