1fc5dd2f97000021e290884d7cb5456d5ee9625f
Sven Strittmatter <ich@weltraumschaf.de>
2014-12-31 00:20:26 +0100

    Add integration test stubs.

--- src/test/java/integrationtests/PublishTest.java
+++ src/test/java/integrationtests/PublishTest.java
@@ -21,7 +21,6 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
