d295b03bb13c775cd357ab310163478f0597f1c6
Sven Strittmatter <ich@weltraumschaf.de>
2014-12-31 13:51:00 +0100

    Increase coverage.

--- src/test/java/de/weltraumschaf/juberblog/core/DirectoriesTest.java
+++ src/test/java/de/weltraumschaf/juberblog/core/DirectoriesTest.java
@@ -9,23 +9,88 @@
  *
  * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
  */
-
 package de.weltraumschaf.juberblog.core;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import nl.jqno.equalsverifier.EqualsVerifier;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import static org.mockito.Mockito.mock;
 
 /**
  * Tests for {@link Directories}.
- * 
+ *
  * @author Sven Strittmatter <weltraumschaf@googlemail.com>
  */
 public class DirectoriesTest {
+
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
+
+    private final Directories sut = new Directories(
+            Paths.get("/blog/data"),
+            Paths.get("/blog/public"));
+
+    @Test
+    public void construct_withNullAsDataDir() {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("dataDir");
+
+        new Directories(null, mock(Path.class));
+    }
+
+    @Test
+    public void construct_withNullAsOutputDir() {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("outputDir");
+
+        new Directories(mock(Path.class), null);
+    }
 
     @Test
     public void equalsContract() {
         EqualsVerifier.forClass(Directories.class)
                 .verify();
+    }
+
+    @Test
+    public void toStringContainsAllProperties() {
+        assertThat(sut.toString(), is(
+                "Directories{"
+                + "postsData=/blog/data/posts, "
+                + "sitesData=/blog/data/sites, "
+                + "output=/blog/public, "
+                + "postsOutput=/blog/public/posts, "
+                + "siteOutput=/blog/public/sites}"));
+    }
+
+    @Test
+    public void getPostsData() {
+        assertThat(sut.getPostsData().toString(), is("/blog/data/posts"));
+    }
+
+    @Test
+    public void getSitesData() {
+        assertThat(sut.getSitesData().toString(), is("/blog/data/sites"));
+    }
+
+    @Test
+    public void getOutput() {
+        assertThat(sut.getOutput().toString(), is("/blog/public"));
+    }
+
+    @Test
+    public void getPostsOutput() {
+        assertThat(sut.getPostsOutput().toString(), is("/blog/public/posts"));
+    }
+
+    @Test
+    public void getSitesOutput() {
+        assertThat(sut.getSitesOutput().toString(), is("/blog/public/sites"));
     }
 
 }
--- src/test/java/de/weltraumschaf/juberblog/core/TemplatesTest.java
+++ src/test/java/de/weltraumschaf/juberblog/core/TemplatesTest.java
@@ -9,23 +9,167 @@
  *
  * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
  */
-
 package de.weltraumschaf.juberblog.core;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import nl.jqno.equalsverifier.EqualsVerifier;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import static org.mockito.Mockito.mock;
 
 /**
  * Tests for {@link Templates}.
- * 
+ *
  * @author Sven Strittmatter <weltraumschaf@googlemail.com>
  */
 public class TemplatesTest {
+
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
+
+    private final Templates sut = new Templates(
+            Paths.get("layoutTemplate"),
+            Paths.get("postTemplate"),
+            Paths.get("siteTemplate"),
+            Paths.get("feedTemplate"),
+            Paths.get("indexTemplate"),
+            Paths.get("siteMapTemplate")
+    );
+
+    @Test
+    public void construct_withNullAsLayoutTemplate() {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("layoutTemplate");
+
+        new Templates(
+                null,
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class));
+    }
+
+    @Test
+    public void construct_withNullAsPostTemplate() {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("postTemplate");
+
+        new Templates(
+                mock(Path.class),
+                null,
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class));
+    }
+
+    @Test
+    public void construct_withNullAsSiteTemplate() {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("siteTemplate");
+
+        new Templates(
+                mock(Path.class),
+                mock(Path.class),
+                null,
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class));
+    }
+
+    @Test
+    public void construct_withNullAsFeedTemplate() {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("feedTemplate");
+
+        new Templates(
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class),
+                null,
+                mock(Path.class),
+                mock(Path.class));
+    }
+
+    @Test
+    public void construct_withNullAsIndexTemplate() {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("indexTemplate");
+
+        new Templates(
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class),
+                null,
+                mock(Path.class));
+    }
+
+    @Test
+    public void construct_withNullAsSiteMapTemplate() {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("siteMapTemplate");
+
+        new Templates(
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class),
+                mock(Path.class),
+                null);
+    }
 
     @Test
     public void equalsContract() {
         EqualsVerifier.forClass(Templates.class)
                 .verify();
+    }
+
+    @Test
+    public void toStringContainsAllProperties() {
+        assertThat(sut.toString(), is(
+                "Templates{"
+                + "layoutTemplate=layoutTemplate, "
+                + "postTemplate=postTemplate, "
+                + "siteTemplate=siteTemplate, "
+                + "feedTemplate=feedTemplate, "
+                + "indexTemplate=indexTemplate, "
+                + "siteMapTemplate=siteMapTemplate}"));
+    }
+
+    @Test
+    public void getLayoutTemplate() {
+        assertThat(sut.getLayoutTemplate().toString(), is("layoutTemplate"));
+    }
+
+    @Test
+    public void getPostTemplate() {
+        assertThat(sut.getPostTemplate().toString(), is("postTemplate"));
+    }
+
+    @Test
+    public void getSiteTemplate() {
+        assertThat(sut.getSiteTemplate().toString(), is("siteTemplate"));
+    }
+
+    @Test
+    public void getFeedTemplate() {
+        assertThat(sut.getFeedTemplate().toString(), is("feedTemplate"));
+    }
+
+    @Test
+    public void getIndexTemplate() {
+        assertThat(sut.getIndexTemplate().toString(), is("indexTemplate"));
+    }
+
+    @Test
+    public void getSiteMapTemplate() {
+        assertThat(sut.getSiteMapTemplate().toString(), is("siteMapTemplate"));
     }
 
 }
