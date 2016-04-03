b6f76822afde6824dfd990696fed08ec7be25d89
Sven Strittmatter <sven.strittmatter@iteratec.de>
2016-04-03 23:33:03 +0200

    Add missing javadoc.

--- src/main/java/de/weltraumschaf/juberblog/options/Options.java
+++ src/main/java/de/weltraumschaf/juberblog/options/Options.java
@@ -3,35 +3,64 @@ package de.weltraumschaf.juberblog.options;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterDescription;
 import de.weltraumschaf.commons.jcommander.JCommanderImproved;
+import de.weltraumschaf.commons.validate.Validate;
 import de.weltraumschaf.juberblog.core.Constants;
 import java.util.List;
-import java.util.Map;
 
 /**
- * Facade to get options.
+ * Facade to get CLI options.
  *
  * @since 1.0.0
  */
 public final class Options {
 
-    private static final String NL = Constants.DEFAULT_NEW_LINE.toString();
-
+    /**
+     * Holds the main program options.
+     */
     private MainOptions main;
+    /**
+     * Holds the install command options.
+     */
     private InstallOptions install;
+    /**
+     * Holds the create command options.
+     */
     private CreateOptions create;
+    /**
+     * Holds the publish command options.
+     */
     private PublishOptions publish;
+    /**
+     * Holds the argument parser.
+     */
     private JCommander parser;
 
+    /**
+     * Dedicated constructor.
+     */
     public Options() {
         super();
         reset();
     }
 
+    /**
+     * Parse the CLI arguments.
+     * <p>
+     * This method {@link #reset() reset} the instance.
+     * </p>
+     *
+     * @param args must not be {@code null}
+     */
     public void parse(final String... args) {
         reset();
-        parser.parse(args);
+        parser.parse(Validate.notNull(args, "args"));
     }
 
+    /**
+     * Determine which command was parsed.
+     *
+     * @return never {@code null}
+     */
     public Command getParsedCommand() {
         final String parsedCommand = parser.getParsedCommand();
 
@@ -42,26 +71,60 @@ public final class Options {
         return Command.valueOf(parsedCommand.toUpperCase());
     }
 
+    /**
+     * Get the main program options.
+     *
+     * @return never {@code null}
+     */
     public MainOptions getMain() {
         return main;
     }
 
+    /**
+     * Get the install command options.
+     *
+     * @return never {@code null}
+     */
     public InstallOptions getInstall() {
         return install;
     }
 
+    /**
+     * Get the create command options.
+     *
+     * @return never {@code null}
+     */
     public CreateOptions getCreate() {
         return create;
     }
 
+    /**
+     * Get the publish command options.
+     *
+     * @return never {@code null}
+     */
     public PublishOptions getPublish() {
         return publish;
     }
 
+    /**
+     * Get the main usage.
+     * <p>
+     * Shorthand for {@link #usage(de.weltraumschaf.juberblog.options.Options.Command)} with {@link Command#NONE}.
+     * </p>
+     *
+     * @return never {@code null} or empty
+     */
     public String usage() {
         return usage(Command.NONE);
     }
 
+    /**
+     * Get usage for command.
+     *
+     * @param cmd may be {@code null}
+     * @return never {@code null} or empty
+     */
     public String usage(final Command cmd) {
         switch (cmd) {
             case INSTALL:
@@ -76,10 +139,24 @@ public final class Options {
         }
     }
 
+    /**
+     * Get the main help.
+     * <p>
+     * Shorthand for {@link #help(de.weltraumschaf.juberblog.options.Options.Command)} with {@link Command#NONE}.
+     * </p>
+     *
+     * @return never {@code null} or empty
+     */
     public String help() {
         return help(Command.NONE);
     }
 
+    /**
+     * Get help for command.
+     *
+     * @param cmd may be {@code null}
+     * @return never {@code null} or empty
+     */
     public String help(final Command cmd) {
         final String usage;
         final String description;
@@ -136,9 +213,26 @@ public final class Options {
         parser.addCommand(Command.PUBLISH.toString(), publish);
     }
 
+    /**
+     * Available commands.
+     */
     public static enum Command {
-
-        NONE, INSTALL, CREATE, PUBLISH;
+        /**
+         * Dummy for no command, but main program.
+         */
+        NONE,
+        /**
+         * Install command.
+         */
+        INSTALL,
+        /**
+         * Create command.
+         */
+        CREATE,
+        /**
+         * Publish command.
+         */
+        PUBLISH;
 
         @Override
         public String toString() {
