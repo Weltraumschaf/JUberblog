bf04d0f085efd58eaef0239848abe9226e98c925
Sven Strittmatter <sven.strittmatter@iteratec.de>
2016-04-05 22:20:13 +0200

    Reduce scope of try.

--- src/main/java/de/weltraumschaf/juberblog/cmd/publish/PublishSubCommand.java
+++ src/main/java/de/weltraumschaf/juberblog/cmd/publish/PublishSubCommand.java
@@ -17,11 +17,6 @@ import org.joda.time.DateTime;
 public final class PublishSubCommand extends SubCommandBase {
 
     /**
-     * Used to execute some tasks in sequence.
-     */
-    private final TaskExecutor executor = new TaskExecutor();
-
-    /**
      * Dedicated constructor.
      *
      * @param registry must not be {@code null}
@@ -32,35 +27,36 @@ public final class PublishSubCommand extends SubCommandBase {
 
     @Override
     protected void doExecute() throws ApplicationException {
+        final TaskExecutor executor = new TaskExecutor()
+            .append(new PublishTask(new PublishTask.Config(
+                        templates(),
+                        directories(),
+                        configuration(),
+                        Page.Type.POST
+                    )))
+            .append(new GenerateFeedTask(new GenerateFeedTask.Config(
+                        templates(),
+                        directories(),
+                        configuration(),
+                        new DateTime("2014-12-08T20:17:00")
+                    )))
+            .append(new GenerateIndexTask(new GenerateIndexTask.Config(
+                        templates(),
+                        directories(),
+                        configuration()
+                    )))
+            .append(new PublishTask(new PublishTask.Config(
+                        templates(),
+                        directories(),
+                        configuration(),
+                        Page.Type.SITE
+                    )))
+            .append(new GenerateSitemapTask(new GenerateSitemapTask.Config(
+                        templates(),
+                        directories(),
+                        configuration())));
         try {
-            executor.append(new PublishTask(new PublishTask.Config(
-                templates(),
-                directories(),
-                configuration(),
-                Page.Type.POST
-            )))
-                .append(new GenerateFeedTask(new GenerateFeedTask.Config(
-                            templates(),
-                            directories(),
-                            configuration(),
-                            new DateTime("2014-12-08T20:17:00")
-                        )))
-                .append(new GenerateIndexTask(new GenerateIndexTask.Config(
-                            templates(),
-                            directories(),
-                            configuration()
-                        )))
-                .append(new PublishTask(new PublishTask.Config(
-                            templates(),
-                            directories(),
-                            configuration(),
-                            Page.Type.SITE
-                        )))
-                .append(new GenerateSitemapTask(new GenerateSitemapTask.Config(
-                            templates(),
-                            directories(),
-                            configuration())))
-                .execute();
+            executor.execute();
         } catch (Exception ex) {
             throw new ApplicationException(ExitCodeImpl.FATAL, ex.getMessage(), ex);
         }
