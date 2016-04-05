19879561e88858e5a19772623fb44128c8f0df72
Sven Strittmatter <sven.strittmatter@iteratec.de>
2016-04-05 19:26:49 +0200

    Add missing test class.

---
+++ src/test/java/de/weltraumschaf/juberblog/cmd/create/CreateSubCommandTest.java
@@ -0,0 +1,18 @@
+
+package de.weltraumschaf.juberblog.cmd.create;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ * Tests for {@link CreateSubCommand}.
+ */
+public class CreateSubCommandTest {
+
+    @Test
+    @Ignore("Write some tests.")
+    public void testSomeMethod() {
+    }
+
+}
